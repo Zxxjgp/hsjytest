ee26a8ec3f462ebd59da31d80c65a4ef1146d4d9
jgp <948102903@qq.com>
2018-10-18 22:38:47 +0800

    jdk代理

--- src/main/java/com/springmvc/proxy/PerformanceMonitor.java
+++ src/main/java/com/springmvc/proxy/PerformanceMonitor.java
@@ -4,17 +4,17 @@ public class PerformanceMonitor {
 	private static ThreadLocal<MethodPerformace> performaceRecord = new ThreadLocal<MethodPerformace>();
 	public static void begin(String method) {
 		System.out.println("begin monitor...");
-		MethodPerformace mp = performaceRecord.get();
+/*		MethodPerformace mp = performaceRecord.get();
 		if(mp == null){
 			mp = new MethodPerformace(method);
 			performaceRecord.set(mp);
 		}else{
-		    mp.reset(method);	
-		}
+		    mp.reset(method);
+		}*/
 	}
 	public static void end() {
 		System.out.println("end monitor...");
-		MethodPerformace mp = performaceRecord.get();
-		mp.printPerformace();
+/*		MethodPerformace mp = performaceRecord.get();
+		mp.printPerformace();*/
 	}
 }
