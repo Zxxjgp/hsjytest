b046e0a1887479ec553f026e6d1041477ee47c4c
jgp <948102903@qq.com>
2018-10-23 17:01:57 +0800

    集成kafka

--- src/main/java/com/springmvc/kafaka/BasicImpl.java
+++ src/main/java/com/springmvc/kafaka/BasicImpl.java
@@ -2,6 +2,7 @@ package com.springmvc.kafaka;
 
 public class BasicImpl implements Basic{
 
+    @Override
     public String sayHello(String name) {
         return "This is Hello words from HESSIAN Server. " + name;
     }
